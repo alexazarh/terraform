ac93612de99ccad19f83180ef3a5257f0424c078
eyalperlm <56922923+eyalperlm@users.noreply.github.com>
2020-02-12 13:59:27 +0200

    Update main.tf

--- azure-mysql/main.tf
+++ azure-mysql/main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
-  version = "=1.28.0"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  subscription_id = "${var.subscription_id}"
-  tenant_id = "${var.tenant_id}"
+  version = "=1.35.0"
 }
 
 resource "azurerm_resource_group" "default" {
